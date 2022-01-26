9ca98342b4253253230256227c388d3410f6eee7
zfabilar <simonfabila@yahoo.com.mx>
2022-01-26 14:17:50 -0600

    Arreglando directiva

--- vistas/js/app/directive/esperaDir.js
+++ vistas/js/app/directive/esperaDir.js
@@ -10,8 +10,8 @@
   	"<div class='modal-dialog modal-dialog-centered' role='document' style='height: auto;'>"+
     "<div class='container' style='height: 100%;'>"+
     "<div class='row' ><div class='col-12' style='text-align: center; align-content: center;' >"+
-  	"<span><img class='spiner mr-4' src='./img/lunaAzul.png' style='height: 50px;width : 50px;'></span>"+
-    "<span><img src='./img/logomooniaart.png' style='width : 150px;'></span></div></div></div></div></div>"
+  	"<span><img class='spiner mr-4' src='./vistas/img/lunaAzul.png' style='height: 50px;width : 50px;'></span>"+
+    "<span><img src='./vistas/img/logomooniaart.png' style='width : 150px;'></span></div></div></div></div></div>"
 		};
 	}]);
 })();
